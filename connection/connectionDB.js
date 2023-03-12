3f9006eb7fe2c02b383f813ccab19cb755bb9307
alexine4 <88242759+alexine4@users.noreply.github.com>
2023-03-12 13:56:11 +0200

    add comments

--- connection/connectionDB.js
+++ connection/connectionDB.js
@@ -1,23 +1,26 @@
+// In this file server connected to database
 const Sequelize = require('sequelize')
 
+// Database name
 const nameDB = 'SmartHouse'
 exports.nameDB = nameDB
-
+// Database user login
 const loginDB = 'test'
 exports.loginDB = loginDB
-
+// Database user password
 const passwordDB = '01010203'
 exports.passwordDB = passwordDB
-
+// Database type
 const typeDB = 'mysql'
 exports.typeDB = typeDB
 
+//Function connection to database
 	const sequelize = new Sequelize(
 		nameDB,
 		loginDB,
 		passwordDB,
 		{	
-			host: '127.0.0.1', // default localhost
+			host: '127.0.0.1', // default localhost or 127.0.0.1
 			port: 3306,
 			dialect: typeDB
 		}
@@ -25,6 +28,6 @@ exports.typeDB = typeDB
 	)
 	
 exports.sequelize = sequelize
-
+// Create jwt key
 const jwt = 'jwt-key'
 exports.jwt = jwt
