128ffd1f4e52d7cba3c925e471a0ac5ac084a0b2
alexine4 <88242759+alexine4@users.noreply.github.com>
2023-04-26 14:15:32 +0300

    add svg and import svg sprite style

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 const routes: Routes = [
   {
     path: '', component: AuthLayoutComponent,
     children: [
       { path: '', redirectTo: '/login', pathMatch: 'full' },
-      { path: 'login', component: LoginPageComponent }
-      // { path: 'register', component: RegisterPageComponent }
+      { path: 'login', component: LoginPageComponent },
+      { path: 'register', component: RegisterPageComponent }
     ]
   }
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthLayoutComponent,
     LoginPageComponent,
-    SiteLayoutComponent
+    SiteLayoutComponent,
+    RegisterPageComponent
   ],
   imports: [
     AppRoutingModule,
