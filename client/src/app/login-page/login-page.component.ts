3aeb3bb6ec14696909a46e0bb403f1d2da38e825
alexine4 <88242759+alexine4@users.noreply.github.com>
2023-05-01 22:26:18 +0300

    add labels and animation

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit, OnDestroy {
+
+  loading = false
+
+  constructor(
+    private authService: AuthService,
+    private toast: ToastrService,
+    private router: Router
+  ) { }
+
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = true
+    }, 2500);
+  }
+
+  public ngOnDestroy(): void {
+    
+  }
 
 }
