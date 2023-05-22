fd886bfdba02000cdd08889d6a1b20d39ea69558
alexine4 <88242759+alexine4@users.noreply.github.com>
2023-05-22 22:23:31 +0300

    change loader

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RoomService } from '../shared/services/room.service';
+import { Observable, Subscription } from 'rxjs';
+import { roomAndType } from '../shared/interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  roomSub$!: Subscription
+  rooms: roomAndType[] = []
+  loading = true
+
+  constructor (
+    private roomService: RoomService
+  ){
+
+  }
+  ngOnInit(): void {
+    
+    this.roomSub$ = this.roomService.fetchWithType().subscribe(
+      room=>{
+        this.rooms.push(room)
+        this.loading = false
+      }
+    )
+
+  }
 }
