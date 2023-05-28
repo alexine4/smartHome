fa9dc4f6d5e40197f02d946118d3120d1db52994
alexine4 <88242759+alexine4@users.noreply.github.com>
2023-05-28 11:53:32 +0300

    create scenario temp service and add functions

---
+++ client/src/app/shared/services/scenario-temp.service.ts
@@ -0,0 +1,32 @@
+import { Observable, delay } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Message, ScenarionTemp } from '../interfaces';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScenarioTempService {
+  delay = 2000
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public fetchByRoom(roomId:number):Observable<ScenarionTemp[]>{
+    return this.httpClient.get<ScenarionTemp[]>(`/api/scenarioTemp/getScenarios/${roomId}`).pipe(delay(this.delay))
+  }
+  public fetchById(scenarioId:number):Observable<ScenarionTemp>{
+    return this.httpClient.get<ScenarionTemp>(`/api/scenarioTemp/getScenario/${scenarioId}`).pipe(delay(this.delay))
+  }
+  public create(scenarioTemp:ScenarionTemp):Observable<Message>{
+    return this.httpClient.post<Message>(`/api/scenarioTemp/addNew`,scenarioTemp).pipe(delay(this.delay))
+  }
+  public update(scenarioTemp:ScenarionTemp):Observable<Message>{
+    return this.httpClient.patch<Message>(`/api/scenarioTemp/update/${scenarioTemp.scenarioId}`,scenarioTemp).pipe(delay(this.delay))
+  }
+  public delete(scenarioId:number):Observable<Message>{
+    return this.httpClient.delete<Message>(`/api/scenarioTemp/delete/${scenarioId}`).pipe(delay(this.delay))
+  }
+
+}
