5df3882b33a3430d3602113034f1aeb0ff67ad27
alexine4 <88242759+alexine4@users.noreply.github.com>
2023-05-28 11:29:20 +0300

    end create scenario temp route

--- routes/scenarioTemp.js
+++ routes/scenarioTemp.js
@@ -8,8 +8,8 @@ const router = express.Router()
 router.get('/getScenarios/:roomId', passport.authenticate('jwt', { session: false }), controller.getAll)
 router.get('/getScenario/:scenarioId', passport.authenticate('jwt', { session: false }), controller.getById)
 router.post('/addNew', passport.authenticate('jwt', { session: false }), controller.addNew)
-router.patch('/update/:roomId', passport.authenticate('jwt', { session: false }), controller.updateByRoom)
-router.delete('/delete/:roomId', passport.authenticate('jwt', { session: false }), controller.deleteById)
+router.patch('/update/:scenarioId', passport.authenticate('jwt', { session: false }), controller.updateById)
+router.delete('/delete/:scenarioId', passport.authenticate('jwt', { session: false }), controller.deleteById)
 
 
 
